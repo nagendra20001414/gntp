6f93ffcb824a1d9ebe88dea522fd35c006c785c0
nagendra20001414 <chinmai.sai@amplussolar.com>
2020-12-30 17:20:31 +0530

    Update io.py
    hjvg

--- gntp/io.py
+++ gntp/io.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import logging
 
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
 
@@ -19,13 +21,16 @@ def iopen(file, *args, **kwargs):
     return f(file, *args, **kwargs)
 
 
-def read_triples(path):
-    triples = []
-    with iopen(path, 'rt') as f:
-        for line in f.readlines():
-            s, p, o = line.split()
-            triples += [(s.strip(), p.strip(), o.strip())]
-    return triples
+def read_triples(path: str):
+    # triples = []
+    df = pd.read_csv(path, sep='\t', names=['s', 'p', 'o'], dtype={'s':str, 'p':str, 'o':str})
+    # with open(path, 'rt') as f:
+    #     for line in f.readlines():
+    #         s, p, o = line.split()
+    #         triples += [(s.strip(), p.strip(), o.strip())]
+    # return triples
+    # df = df.astype(str)
+    return df.values.tolist()[:int(fraction*len(df))]
 
 
 def read_mentions(path):
